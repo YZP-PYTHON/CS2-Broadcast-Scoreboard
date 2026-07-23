050ed1c2b533927ae4afd65f1824f419daea3d32
yzp-python <yzp2369114@163.com>
2026-07-23 10:32:00 +0000

    feat:实现剩余时间与队名，比分显示 TODO 接入后端api，实时更新数据

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,117 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 export default function Home() {
+  const [time, setTime] = useState("0:10");
+  const [team1, setTeam1] = useState("TeamA")
+  const [team2, setTeam2] = useState("TeamB")
+  const [score, setScore] = useState([0,1])
+  const [scoreColor,setScoreColor] = useState({
+    win:
+      process.env.NEXT_PUBLIC_SCORE_WIN 
+      ?? "#ef4444",
+
+    lose:
+      process.env.NEXT_PUBLIC_SCORE_LOSE
+      ?? "#3b82f6",
+
+    draw:
+      process.env.NEXT_PUBLIC_SCORE_DRAW
+      ?? "#ffffff"
+  });
+  const [scoreColorNow ,setScoreColorNow] = useState({
+    CT : "#ffffff",
+    T : "#ffffff"
+  })
+  
+
+  useEffect(() => {
+
+  console.log(
+    "score changed:",
+    score
+  );
+
+  console.log(
+    "color config:",
+    scoreColor
+  );
+
+
+  if(score[0] > score[1]){
+
+      console.log("CT win");
+
+      setScoreColorNow({
+        CT: scoreColor.win,
+        T: scoreColor.lose
+      });
+
+    }
+    else if(score[0] < score[1]){
+
+      console.log("T win");
+
+      setScoreColorNow({
+        CT: scoreColor.lose,
+        T: scoreColor.win
+      });
+
+    }
+    else{
+
+      console.log("draw");
+
+      setScoreColorNow({
+        CT: scoreColor.draw,
+        T: scoreColor.draw
+      });
+
+    }
+
+  },[score,scoreColor]);
+  
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="relative w-full h-full">
+
+    {/* 背景层 */}
+    <div className="absolute inset-0 bg-black blur-3xl z-1 opacity-[0.90]">
+      
     </div>
+
+    {/* 内容层 */}
+    <div className="relative z-10 flex-col items-center justify-center mt-[63px] h-screen">
+      <div className="text-white text-[123px] [text-shadow:0_2px_10px_rgba(0,0,0,0.8)] flex fixed top-[73px] left-1/2 -translate-x-1/2 leading-none">
+        {time}
+      </div>
+      <div className="relative w-screen h-[390px]">
+
+        {/* 左队 */}
+        <div className="absolute left-[200px] top-1/2 -translate-y-1/2 text-[96px] text-white">
+          {team1}
+        </div>
+
+        {/* 比分 */}
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <div className="flex items-center text-[103px] text-white gap-5">
+            <div style={{color: scoreColorNow.CT}}>{score[0]}</div>
+            <div>:</div>
+            <div style={{color: scoreColorNow.T}}>{score[1]}</div>
+          </div>
+        </div>
+
+        {/* 右队 */}
+        <div className="absolute right-[200px] top-1/2 -translate-y-1/2 text-[96px] text-white">
+          {team2}
+        </div>
+
+      </div>
+    </div>
+    
+</div>
+    
+    
   );
 }
