98a7ce1d68c7d4a3eb6f24ccab56bfd88fbc87e6
yzp-python <yzp2369114@163.com>
2026-07-24 13:00:22 +0000

    feat:添加动画

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
         if (data.phase === "over"){
           setTime("回合结束")
           setPaused(false)
-          setIsBlink(true)
+          // setIsBlink(true)
           setShowScoreboard(true)
           setTimeColor("#ffffff")
         }else if(data.phase === "paused"){
@@ -240,8 +240,8 @@ export default function Home() {
 
   return (
     <div className="relative w-full h-full">
-    {showScorebord &&(
-    <div>
+
+    <div className={`${showScorebord ? "scoreboard-show" : "scoreboard-hide"}`}>
       <div className="absolute inset-0 bg-black blur-3xl z-1 opacity-[0.90]">
         
       </div>
@@ -250,10 +250,18 @@ export default function Home() {
       <div className={`relative z-10 flex-col items-center justify-center mt-[63px] h-screen `}>
         <div style={{color:timeColor}} className={`text-[123px] [text-shadow:0_2px_10px_rgba(0,0,0,0.8)]
          flex fixed top-[73px] left-1/2 -translate-x-1/2 leading-none 
-         ${isBlink ? "animate-[blink_1000ms_steps(1)_infinite]" : ""}`}>
-          {time}
+         ${isBlink ? "animate-[blink_1000ms_steps(1)_infinite]" : ""} ${showScorebord?"timmer-text-show":"timmer-text-hide"}`}>
+          
+          <span style={{color:timeColor}} className={`${isBlink
+              ? "animate-[blink_1000ms_steps(1)_infinite]"
+              : ""}
+            `}
+          >
+            {time}
+          </span>
+          
         </div>
-        <div className="relative w-screen h-[390px]">
+        <div className={`relative w-screen h-[390px] ${showScorebord?"score-text-show":"score-text-hide"}`}>
 
           {/* 左队 */}
           <div className="absolute left-[200px] top-1/2 -translate-y-1/2 text-[96px] text-white">
@@ -276,19 +284,15 @@ export default function Home() {
 
         </div>
         <div >
-          <div className=" absolute left-[119px] top-[285px]">
-            <Scoreboard players = {scoreboard[0]} data = {scoreboardItemConfig} color={"from-blue-600 to-blue-400"} />
+          <div className={` absolute left-[119px] top-[285px] ${showScorebord ? "" : "table-hide" } `}>
+            <Scoreboard players = {scoreboard[0]} data = {scoreboardItemConfig} color={"from-blue-600 to-blue-400"} animate={showScorebord} />
           </div>
-          <div className="absolute right-[120px] top-[285px]">
-            <Scoreboard players= {scoreboard[1]} data={scoreboardItemConfig} color={"bg-gradient-to-r from-red-600 to-red-400"}/>
+          <div className={`absolute right-[120px] top-[285px] ${showScorebord ? "" : "table-hide" } `}>
+            <Scoreboard players= {scoreboard[1]} data={scoreboardItemConfig} color={"bg-gradient-to-r from-red-600 to-red-400"} animate={showScorebord}/>
           </div>
         </div>
       </div>
     </div>
-      
-    )}
-
-      
   </div>
     
     
--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,10 +1,20 @@
-export function Scoreboard({players,data,color}){
+export function Scoreboard({players,data,color,animate}){
 
     console.log("scoreboard",data);
     console.log("scoreboard",Array.isArray(data));
     console.log("scoreboard",typeof data);
     return(
-        <div className="">
+        <div className={`
+            origin-top
+            transition-transform
+            duration-1500
+
+            ${
+                animate
+                ?"scale-y-100"
+                :""
+            }
+        `}>
             <table className={`w-[790px] border-separate border-spacing-0
             text-white text-[35px] border-white/60 mb-[30px] rounded-3xl 
             shadow-[0_0_40px_rgba(0,0,0,0.4)] bg-[#001A35]/80 border overflow-hidden`}>
@@ -25,13 +35,24 @@ export function Scoreboard({players,data,color}){
                     {players?.map((player) => (
                         <tr
                             key={player.steamId}
-                            className="relative
+                            className={`relative
                             after:absolute
                             after:left-5
                             after:right-5
                             after:top-0
                             after:h-[2pt]
-                            after:bg-black/50"
+                            after:bg-black/50
+                            ${
+                                    animate
+                                    ? "player-row-show"
+                                    : ""
+                                }
+                            `}
+                            style={{
+                                animationDelay: animate
+                                ? `${players.indexOf(player) * 300}ms`
+                                : `${(players.length - players.indexOf(player)) * 50}ms`
+                            }}
                         >
                             
                             <td className="px-5 py-6">{player.name}</td>
