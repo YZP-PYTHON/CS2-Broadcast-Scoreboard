a4090f0626c037a39587d2b207c7d5291d268fb8
yzp-python <yzp2369114@163.com>
2026-08-18 09:49:02 +0000

    fix:玩家名称过长会导致意外换行 #7

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -20,12 +20,12 @@ export function Scoreboard({players,data,color,animate,excludePlayer}){
         `}>
             <table className={`w-[790px] border-separate border-spacing-0
             text-white text-[35px] border-white/60 mb-[30px] rounded-3xl 
-            shadow-[0_0_40px_rgba(0,0,0,0.4)] bg-[#001A35]/80 border overflow-hidden`}>
-            <thead className={`bg-gradient-to-r ${color} rounded-t-3xl`}>
+            shadow-[0_0_40px_rgba(0,0,0,0.4)] bg-[#001A35]/80 border overflow-hidden `}>
+            <thead className={`bg-gradient-to-r ${color} rounded-t-3xl whitespace-nowrap`}>
                 <tr className="border-b border-white/20 py-2">
-                <th className="px-[50px] py-3 text-left rounded-tl-3xl">选手</th>
+                <th className="px-[50px] py-3 text-left rounded-tl-3xl whitespace-nowrap">选手</th>
                 {data?.slice(0, 5).map((item)=>(
-                    <th className="px-4 py-3 text-center" key={item.key}>{item.name}</th>
+                    <th className="px-4 py-3 text-center whitespace-nowrap" key={item.key}>{item.name}</th>
                     
                 ))}
                 
@@ -58,9 +58,9 @@ export function Scoreboard({players,data,color,animate,excludePlayer}){
                             }}
                         >
                             
-                            <td className="px-5 py-6">{player.name}</td>
+                            <td className="px-5 py-6 whitespace-nowrap overflow-hidden text-ellipsis">{player.name}</td>
                             {data?.slice(0,5).map((item)=>(
-                                <td className="px-5 py-6 text-center" key={item.key}>
+                                <td className="px-5 py-6 text-center " key={item.key}>
                                     {player[item.key]?.toFixed(Number(item.fix))}
                                 </td>
                                 
